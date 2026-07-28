fba813a28d4d672d848ed7df7f0ed149d38920cc
Virsha Arif <217931360+virshaarif@users.noreply.github.com>
2026-07-28 13:52:36 +0500

    chnages add

--- script.js
+++ script.js
@@ -21,3 +21,16 @@
 	enableValidationRedirect('loginForm', 'dashboard.html');
 	enableValidationRedirect('signupForm', 'login.html');
 })();
+const mobileNav = document.getElementById('mobileNav');
+const menuBtn = document.getElementById('menuBtn');
+const icon = menuBtn.querySelector('i');
+
+mobileNav.addEventListener('shown.bs.offcanvas', () => {
+    icon.classList.remove('bi-list');
+    icon.classList.add('bi-x-lg');
+});
+
+mobileNav.addEventListener('hidden.bs.offcanvas', () => {
+    icon.classList.remove('bi-x-lg');
+    icon.classList.add('bi-list');
+});
